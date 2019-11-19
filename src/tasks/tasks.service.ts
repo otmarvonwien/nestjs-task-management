b2a5277b9cedaa6d68a68061de0c444b802e1996
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-11-19 20:35:12 +0100

    Authorization: Getting Tasks For User

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,8 +14,11 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
-    return this.taskRepository.getTasks(filterDto);
+  async getTasks(
+    filterDto: GetTaskFilterDto,
+    user: User,
+    ): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto, user);
   }
 
   async getTaskById(id: number): Promise<Task> {
